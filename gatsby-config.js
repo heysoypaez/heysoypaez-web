893db24569c84861fdd0558200fb432b838ebdfb
Daniel Páez <danielpaezsw66@gmail.com>
2020-02-07 23:18:41 -0300

    Google Analytics track added

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-115468844-5`,
       },
     },
     `gatsby-plugin-feed`,
